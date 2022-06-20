d7b3a9ba28ced4e0bc4c3410f2bb873bbd44960b
Apurv Pandey <apurvpandey1306@gmail.com>
2022-06-20 18:38:17 +0100

    test

--- PA.py
+++ PA.py
@@ -1,1 +1,16 @@
-print("hello world")
+import kivy
+from kivy.app import App
+from kivy.uix.widget import Widget
+
+
+class Test(Widget):
+    pass
+
+
+class TestApp(App):
+    def build(self):
+        return Test()
+
+
+if __name__ == '__main__':
+    TestApp().run()
