c24f52267ca7ec0674e5ce64e3210b617af5de5d
surabhi-paliwal <surabhi.paliwal314@gmail.com>
2022-06-20 23:08:58 +0530

    no one will believe you

--- PA.py
+++ PA.py
@@ -1,2 +1,18 @@
-print("hello world")
-print("surabhi is here")
+import kivy
+from kivy.app import App
+from kivy.uix.widget import Widget
+
+
+class Test(Widget):
+    pass
+
+
+class TestApp(App):
+    def build(self):
+        return Test()
+
+
+if __name__ == '__main__':
+    TestApp().run()
+
+    print("i am in your walls")
