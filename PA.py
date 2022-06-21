f437f21ec53bff5d19b195ed8a61650d3ed69b10
Apurv Pandey <apurvpandey1306@gmail.com>
2022-06-21 18:31:49 +0100

    first page

--- PA.py
+++ PA.py
@@ -1,18 +1,34 @@
+
 import kivy
 from kivy.app import App
-from kivy.uix.widget import Widget
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.screenmanager import Screen, ScreenManager
+from kivy.uix.label import Label
+from kivy.uix.button import Button
 
 
-class Test(Widget):
-    pass
+class IntroPage(GridLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
 
+        self.rows = 2
+# adding the label
+        self.add_widget(Label(text = "Welcome To AVA"))
+#adding the button
+        self.continue_butt = Button(text = "continue")
+        self.add_widget(self.continue_butt)
 
-class TestApp(App):
+class PAApp(App):
     def build(self):
-        return Test()
+        self.screen_manager = ScreenManager()
 
+        self.page1 = IntroPage()
+        screen = Screen(name="intro")
+        screen.add_widget(self.page1)
+        self.screen_manager.add_widget(screen)
 
-if __name__ == '__main__':
-    TestApp().run()
+        return self.screen_manager
 
-print("hello work")
+if __name__ == "__main__":
+    pa_app = PAApp()
+    pa_app.run()
